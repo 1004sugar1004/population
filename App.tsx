e499d16159a983a7b5d6374da9399399a4c136e0
1004sugar1004 <1004sugar1004@gmail.com>
2025-08-27 11:54:19 +0900

    refactor: Remove unused import in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { PRELOADED_DATA } from './constants';
 import type { PopulationData, SavedChart } from './types';
